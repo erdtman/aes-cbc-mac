fb816bcfde1d94b341225eafa2a096f641a93e75
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2021-06-01 20:39:31 +0200

    Switch to isomorphic-webcrypto

--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,16 @@
-const crypto = require('crypto');
+const crypto: Crypto = require('isomorphic-webcrypto');
 
 const iv = new Uint8Array(16); // Initialization vector.
 
-export function create(key: Uint8Array, msg: Uint8Array, len: 8 | 16) {
-  const algorithm = `aes-${key.length * 8}-cbc`;
-
-  const msgLen = msg.length;
-  const padLen = msgLen % 16 ? 16 - (msgLen % 16) : 0;
+export async function create(key: Uint8Array, msg: Uint8Array, len: 8 | 16): Promise<ArrayBuffer> {
+  const padLen = msg.length % 16 ? 16 - (msg.length % 16) : 0;
   const paddedMsg = new Uint8Array(msg.length + padLen);
   paddedMsg.set(msg, 0);
 
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
-  const enc = cipher.update(paddedMsg);
-  const tagStart = enc.length - 16;
-  const tag = enc.slice(tagStart, tagStart + len);
+  const crypto_key = await crypto.subtle.importKey("raw", key, { name: "AES-CBC" }, false, ["encrypt"]);
+  const enc = await crypto.subtle.encrypt({ name: "AES-CBC", iv }, crypto_key, paddedMsg);
 
+  const tagStart = enc.byteLength - 16 - 16;
+  const tag = enc.slice(tagStart, tagStart + len);
   return tag;
 };
--- test/aes-cbc-mac.js
+++ test/aes-cbc-mac.js
@@ -4,40 +4,40 @@
 const test = require('ava');
 const aesCbcMac = require('../build/index');
 
-test('AES-MAC-128/64', (t) => {
+test('AES-MAC-128/64', async (t) => {
   const msg = Buffer.from('84634D414343A1010E4054546869732069732074686520636F6E74656E742E', 'hex');
   const key = Buffer.from('849B57219DAE48DE646D07DBB533566E', 'hex');
 
-  const tag = aesCbcMac.create(key, msg, 8);
-  t.is(tag.toString('hex').toLocaleUpperCase(), 'C1CA820E6E247089');
+  const tag = await aesCbcMac.create(key, msg, 8);
+  t.deepEqual(Buffer.from(tag).toString('hex').toUpperCase(), 'C1CA820E6E247089');
 });
 
-test('AES-MAC-128/128', (t) => {
+test('AES-MAC-128/128', async (t) => {
   const msg = Buffer.from('84634D414344A10118194054546869732069732074686520636F6E74656E742E', 'hex');
   const key = Buffer.from('849B57219DAE48DE646D07DBB533566E', 'hex');
 
-  const tag = aesCbcMac.create(key, msg, 16);
-  t.is(tag.toString('hex').toLocaleUpperCase(), 'B242D2A935FEB4D66FF8334AC95BF72B');
+  const tag = await aesCbcMac.create(key, msg, 16);
+  t.is(Buffer.from(tag).toString('hex').toUpperCase(), 'B242D2A935FEB4D66FF8334AC95BF72B');
 });
 
-test('AES-MAC-256/64', (t) => {
+test('AES-MAC-256/64', async (t) => {
   const msg = Buffer.from('84634D414343A1010F4054546869732069732074686520636F6E74656E742E', 'hex');
   const key = Buffer.from('849B57219DAE48DE646D07DBB533566E976686457C1491BE3A76DCEA6C427188', 'hex');
 
-  const tag = aesCbcMac.create(key, msg, 8);
-  t.is(tag.toString('hex').toLocaleUpperCase(), '9E1226BA1F81B848');
+  const tag = await aesCbcMac.create(key, msg, 8);
+  t.is(Buffer.from(tag).toString('hex').toUpperCase(), '9E1226BA1F81B848');
 });
 
-test('AES-MAC-256/128', (t) => {
+test('AES-MAC-256/128', async (t) => {
   const msg = Buffer.from('84634D414344A101181A4054546869732069732074686520636F6E74656E742E', 'hex');
   const key = Buffer.from('849B57219DAE48DE646D07DBB533566E976686457C1491BE3A76DCEA6C427188', 'hex');
 
-  const tag = aesCbcMac.create(key, msg, 16);
-  t.is(tag.toString('hex').toLocaleUpperCase(), 'DB9C7598A0751C5FF3366B6205BD2AA9');
+  const tag = await aesCbcMac.create(key, msg, 16);
+  t.is(Buffer.from(tag).toString('hex').toUpperCase(), 'DB9C7598A0751C5FF3366B6205BD2AA9');
 });
 
-test('AES-MAC invalid key lenght', (t) => {
+test('AES-MAC invalid key lenght', async (t) => {
   const msg = Buffer.from('84', 'hex');
   const key = Buffer.from('849B57219DAE48DE646D07DBB533566E97668', 'hex');
-  t.throws(() => aesCbcMac.create(key, msg, 16));
+  await t.throwsAsync(() => aesCbcMac.create(key, msg, 16));
 });
